8073df9da691fb960350b1022bc71b9b1d5d2de5
Aaron-McCann <aamccann@tcd.ie>
2024-09-18 16:19:13 +0100

    update from work laptop

--- Week 2/week2.py
+++ Week 2/week2.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
-df= pd.read_csv("week2.csv" )
+#df= pd.read_csv("week2.csv" )
+df = pd.read_csv('C:/Users/McCannA/Desktop/Machine Learning/Machine-Learning/Week 2/week2.csv')
+
 print( df.head( ) )
 
 df.columns = ('X1', 'X2', 'y')
@@ -28,7 +30,7 @@ from sklearn.linear_model import LogisticRegression
 X = np.column_stack((df['X1'], df['X2']))
 Y = df['y']
 
-model = LogisticRegression(penalty= 'none', solver='lbfgs')
+model = LogisticRegression(penalty= None , solver='lbfgs')
 model.fit(X,Y)
 
 
@@ -46,6 +48,15 @@ predicted = model.predict(X)
 p_class1 = df[predicted == -1]  # Predicted class -1
 p_class2 = df[predicted == 1]   # Predicted class +1
 
+w1 = model.coef_[0][0]
+w2 = model.coef_[0][1]
+
+b = model.intercept_[0]
+#TODO: change this
+range = np.linspace(-10, 10, 100)
+boundry = -((w1*range + b) / w2)
+#y_values = -(model.coef_[0][0] * range + model.intercept_[0]) / model.coef_[0][1]
+matplotlib.pyplot.plot(range, boundry, 'k-', label='Decision Boundary')
 
 matplotlib.pyplot.scatter(class1['X1'], class1['X2'], marker = 'o', color='r', label='+1'  )
 matplotlib.pyplot.scatter(class2['X1'], class2['X2'], marker = '+', color='b', label='+1'  )
@@ -61,3 +72,23 @@ matplotlib.pyplot.show()
 
 
 #edit to show it working 
+
+#b
+from sklearn.svm import LinearSVC
+
+#SVM = LinearSVC()
+
+cval = [0.001, 1, 100]
+svms = []
+for c in cval:
+    SVM = LinearSVC(C=c)
+    SVM.fit(X,Y)
+    svms.append(SVM)
+    print(f"C={c}:")
+    print(f"Coefficients:{SVM.coef_}")
+    print(f"Intercept = {SVM.intercept_}")
+
+
+
+
+#class sklearn.svm.LinearSVC(penalty='l2', loss='squared_hinge', *, dual='auto', tol=0.0001, C=1.0, multi_class='ovr', fit_intercept=True, intercept_scaling=1, class_weight=None, verbose=0, random_state=None, max_iter=1000)[source]
